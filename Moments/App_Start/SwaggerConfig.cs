32018d82dd506057183bea1173c7b5ffb54fe785
Manuel Ivan Alzamora Flores <manolo_1505@hotmail.com>
2017-07-06 15:26:03 -0500

    for local

--- Moments/App_Start/SwaggerConfig.cs
+++ Moments/App_Start/SwaggerConfig.cs
@@ -20,32 +20,32 @@ namespace Moments
                 .EnableSwagger(c =>
                 {
 
-                    c.SingleApiVersion("v1", "Moments");
-                    c.IncludeXmlComments(string.Format(@"{0}\bin\Moments.XML",
-                       System.AppDomain.CurrentDomain.BaseDirectory));
-                    c.OAuth2("oauth2")
-                        .Description("OAuth2 Implicit Grant")
-                        .Flow("implicit")
-                        .AuthorizationUrl("http://silspapi.azurewebsites.net/Token")
-                        .TokenUrl("http://silspapi.azurewebsites.net/Token")
-                        .Scopes(scopes =>
-                        {
-                            scopes.Add("roles", "Write access to protected resources");
-                        });
+                    c.SingleApiVersion("v1", "Moment");
+                     //c.IncludeXmlComments(string.Format(@"{0}\bin\Moments.XML",
+                     //   System.AppDomain.CurrentDomain.BaseDirectory));
+                     //c.OAuth2("oauth2")
+                     //    .Description("OAuth2 Implicit Grant")
+                     //    .Flow("implicit")
+                     //    .AuthorizationUrl("http://silspapi.azurewebsites.net/Token")
+                     //    .TokenUrl("http://silspapi.azurewebsites.net/Token")
+                     //    .Scopes(scopes =>
+                     //    {
+                     //        scopes.Add("roles", "Write access to protected resources");
+                     //    });
 
-                    c.OperationFilter<AssignOAuth2SecurityRequirements>();
+                     c.OperationFilter<AssignOAuth2SecurityRequirements>();
                 })
 
                 .EnableSwaggerUi(c =>
                 {
-                    c.EnableDiscoveryUrlSelector();
-                    c.EnableOAuth2Support(
-                        clientId: "test-client-id",
-                        clientSecret: null,
-                        realm: "test-realm",
-                        appName: "Moments"
-                    //additionalQueryStringParams: new Dictionary<string, string>() { { "foo", "bar" } }
-                    );
+                    //c.EnableDiscoveryUrlSelector();
+                    //c.EnableOAuth2Support(
+                    //    clientId: "test-client-id",
+                    //    clientSecret: null,
+                    //    realm: "test-realm",
+                    //    appName: "Moments"
+                    ////additionalQueryStringParams: new Dictionary<string, string>() { { "foo", "bar" } }
+                    //);
                 });
         }
     }
